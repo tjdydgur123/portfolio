81ecf614f299640b8430688b1ee035c9ff37953e
YongHyuk <tjdydgur123@gmail.com>
2021-04-29 19:03:15 -0700

    JS: Navbar

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 "use strict";
-console.log("Hello World!");
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector("#navbar"); // get the element navbar
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  // if scrolling is larger than the navbar height
+  if (window.scrollY > navbarHeight) {
+    // add class -> check the dev tool
+    navbar.classList.add("navbar--dark");
+  } else {
+    // remove class
+    navbar.classList.remove("navbar--dark");
+  }
+});
